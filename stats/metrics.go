b00763dee06ca95e6e1034c5f6c35896763621bf
Masih H. Derkani <m@derkani.org>
2025-08-14 13:30:12 +0100

    Set bucket boundaries for histogram metrics
    Set appropriate bucket boundaries for histogram metrics to accommodate
    the distribution of measured values.

--- stats/metrics.go
+++ stats/metrics.go
@@ -16,7 +16,8 @@ var (
 		gasUsed: must(meter.Int64Histogram(
 			"gas_used",
 			metric.WithDescription("Gas used in transactions"),
-			metric.WithUnit("{gas}"))),
+			metric.WithUnit("{gas}"),
+			metric.WithExplicitBucketBoundaries(1, 1000, 10_000, 50_000, 100_000, 200_000, 300_000, 400_000, 500_000, 600_000, 700_000, 800_000, 1_000_000))),
 		blockNumber: must(meter.Int64Gauge(
 			"block_number",
 			metric.WithDescription("Block number in the chain"),
@@ -24,7 +25,8 @@ var (
 		blockTime: must(meter.Float64Histogram(
 			"block_time",
 			metric.WithDescription("Time taken to produce a block"),
-			metric.WithUnit("s"))),
+			metric.WithUnit("s"),
+			metric.WithExplicitBucketBoundaries(0.001, 0.01, 0.05, 0.1, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0))),
 	}
 )
 
